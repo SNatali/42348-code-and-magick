0444ab1b0711751588f584f9893b9936f09875a3
Natalia Sergieieva <bossovivh@mail.ru>
2018-01-31 11:02:37 +0100

    correct mistake

--- js/stat.js
+++ js/stat.js
@@ -1,14 +1,16 @@
 'use strict';
 
 var CLOUD_WIDTH = 500;
-var CLOUD_HEIGHT = 230;
+var CLOUD_HEIGHT = 250;
 var CLOUD_X = 100;
 var CLOUD_Y = 50;
 var GAP = 10;
+var GAP_COLUM = GAP * 5;
 var FONT_GAP = 40;
 var TEXT_HEIGHT = 20;
-var TEXT_GAP = 255;
+var TEXT_GAP = 275;
 var COLUM_WIDTH = 40;
+var COLUM_MAX = CLOUD_Y * 3;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -40,24 +42,23 @@ window.renderStatistics = function (ctx, names, times) {
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = 'gray';
-    ctx.fillText(names[i], (CLOUD_X + FONT_GAP) + (COLUM_WIDTH * i + GAP * 5 * i), TEXT_GAP);
+    ctx.fillText(names[i], (CLOUD_X + FONT_GAP) + (COLUM_WIDTH * i + GAP_COLUM * i), TEXT_GAP);
     var randomOpacity = Math.random();
     ctx.fillStyle = 'rgba(3, 37, 144,' + randomOpacity + ')';
-    
+
 
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'red';
     }
 
-    var columnHeight = CLOUD_Y * 3 * times[i] / maxTime; // Вот такие вещи нужно выносить в переменные, так как читая формулу, вообще не понятно ее предназначение
-    // Так же все магические числа нужно выносить в переменные, например число 3 в предыдущей строке, вообще не понятно почему 3, для чего оно
+    var columnHeight = COLUM_MAX * times[i] / maxTime;
     var columntYPosition = TEXT_GAP - columnHeight - GAP;
-    // Тоже самое касается числа 5 при расчете ширины, ну и собственно всю формулу расчета ширины лучше вынести так же как я вынес высоту
+    var columnXPosition = COLUM_WIDTH * i + GAP_COLUM * i;
 
-    ctx.fillRect(CLOUD_X + FONT_GAP + (COLUM_WIDTH * i + GAP * 5 * i), columntYPosition, COLUM_WIDTH, columnHeight);
+    ctx.fillRect(CLOUD_X + FONT_GAP + columnXPosition, columntYPosition, COLUM_WIDTH, columnHeight);
 
     // Отображаем статистику
     ctx.fillStyle = 'black';
-    ctx.fillText(Math.round(times[i]), (CLOUD_X + FONT_GAP) + (COLUM_WIDTH * i + GAP * 5 * i), 100);
+    ctx.fillText(Math.round(times[i]), (CLOUD_X + FONT_GAP) + (COLUM_WIDTH * i + GAP_COLUM * i), 100);
   }
 };
