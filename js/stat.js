25b2bb4976c5f1dcc688f28d8051f3abedaf696d
Natalia Sergieieva <bossovivh@mail.ru>
2018-02-02 13:15:16 +0100

    function

--- js/stat.js
+++ js/stat.js
@@ -30,10 +30,17 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+var getcolumnXPosition = function (value) {
+  return COLUM_WIDTH * value + GAP_COLUM * value;
+};
+
+var getcolumnHeight = function (value, MaxElement) {
+  return COLUM_MAX * times[value] / MaxElement;
+};
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
@@ -41,25 +48,24 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов:', CLOUD_X + GAP_DOUBLE, FONT_GAP + TEXT_HEIGHT_DOUBLE);
 
   var maxTime = getMaxElement(times);
+  var columnHeight = getcolumnHeight(i, maxTime);
+
+  // var columnHeight = getcolumnHeight(i, getMaxElement(times) );
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = 'gray';
-    ctx.fillText(names[i], (CLOUD_X + FONT_GAP) + (COLUM_WIDTH * i + GAP_COLUM * i), TEXT_GAP);
+    ctx.fillText(names[i], (CLOUD_X + FONT_GAP) + getcolumnXPosition(i), TEXT_GAP);
     var randomOpacity = Math.random();
     ctx.fillStyle = 'rgba(3, 37, 144,' + randomOpacity + ')';
-
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'red';
     }
-
-    var columnHeight = COLUM_MAX * times[i] / maxTime;
+    // var columnHeight = COLUM_MAX * times[i] / maxTime;
     var columntYPosition = TEXT_GAP - columnHeight - GAP;
-    var columnXPosition = COLUM_WIDTH * i + GAP_COLUM * i;
-
-    ctx.fillRect(CLOUD_X + FONT_GAP + columnXPosition, columntYPosition, COLUM_WIDTH, columnHeight);
-
+    // var columnXPosition = COLUM_WIDTH * i + GAP_COLUM * i;
+    ctx.fillRect(CLOUD_X + FONT_GAP + getcolumnXPosition(i), columntYPosition, COLUM_WIDTH, columnHeight);
     // Отображаем статистику
     ctx.fillStyle = 'black';
-    ctx.fillText(Math.round(times[i]), (CLOUD_X + FONT_GAP) + columnXPosition, columntYPosition - GAP_DOUBLE, COLUM_WIDTH, columnHeight);
+    ctx.fillText(Math.round(times[i]), (CLOUD_X + FONT_GAP) + getcolumnXPosition(i), columntYPosition - GAP_DOUBLE, COLUM_WIDTH, columnHeight);
   }
 };
